dc5d59440ec5dcfd19553c01d637539704932be4
Franco Lucchini <flucchini@gmail.com>
2026-02-06 09:44:04 +0100

    initial import (working as expected)

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use std::collections::HashSet;
+use std::fs::remove_file;
+
+use rusqlite::Connection;
+use xlsx_batch_reader::read::XlsxBook;
+
+const CD_DITTA: usize = 0;
+const RS_DITTA: usize = 1;
+const TIPO: usize = 2;
+const CODICE: usize = 3;
+const RAGSOC: usize = 4;
+///// INDIRIZZO: usize = 5;
+///// CAP: usize = 6;
+///// CITTA: usize = 7;
+///// PROV: usize = 8;
+const PARTITA_IVA: usize = 9;
+const CD_FISCALE: usize = 10;
+
+fn main() -> anyhow::Result<()> {
+    let mut ditte: HashSet<String> = HashSet::new();
+    remove_file("TSPRI.db").ok();
+    let mut db = Connection::open("TSPRI.db")?;
+    db.execute_batch("
+        CREATE TABLE ditte (
+            ragsoc TEXT,
+            ditta INTEGER,
+            PRIMARY KEY (ragsoc)
+        );
+        CREATE TABLE anag (
+            ditta INTEGER,
+            tipo INTEGER,
+            codice INTEGER,
+            ragsoc TEXT,
+            partiv TEXT,
+            codfis TEXT,
+            PRIMARY KEY (ditta, tipo, codice)
+        );
+    ")?;
+    for xf in ["PRICLI.xlsx", "PRIFOR.xlsx"] {
+        let mut wb = XlsxBook::new(xf, true)?;
+        for sn in wb.get_visible_sheets().clone() {
+            let ws = wb.get_sheet_by_name(&sn, 5000, 0, 1, 11, true)?;
+            for batch in ws {
+                let tx = db.transaction()?;
+                if let Ok((_, rows)) = batch {
+                    for rw in rows {
+                        if let Some(ref dsc_ditta) = rw[RS_DITTA].get::<String>()? {
+                            if !ditte.contains(dsc_ditta) {
+                                tx.execute("INSERT INTO ditte (ragsoc, ditta) VALUES (?1, ?2)", (
+                                    dsc_ditta,
+                                    rw[CD_DITTA].get::<i64>()?.unwrap_or_default())
+                                )?;
+                                ditte.insert(dsc_ditta.to_string());
+                            }
+                        }
+                        tx.execute("INSERT INTO anag (ditta, tipo, codice, ragsoc, partiv, codfis) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+                            (
+                                rw[CD_DITTA].get::<i64>()?,
+                                rw[TIPO].get::<i64>()?,
+                                rw[CODICE].get::<i64>()?,
+                                rw[RAGSOC].get::<String>()?,
+                                rw[PARTITA_IVA].get::<String>()?,
+                                rw[CD_FISCALE].get::<String>()?
+                            )
+                        )?;
+                    }
+                }
+                tx.commit()?;
+            }
+        }
+    }
+    Ok(())
+}
